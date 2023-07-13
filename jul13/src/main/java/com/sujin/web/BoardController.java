899c7fa1ba3ac9bac81f84aa46aaf8d2dc112bdd
soojin.kim@altiall.com <soojinkim!1>
2023-07-13 10:32:46 +0900

    MVC 패턴 2

---
+++ jul13/src/main/java/com/sujin/web/BoardController.java
@@ -0,0 +1,18 @@
+package com.sujin.web;
+
+// Ctrl + shift + o --> 임포트 정리해줘요.
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BoardController {
+	
+	//사용자가 /board라고 호출하면 동작할 매소드를 만들겠습니다.
+	
+	@GetMapping("/board")
+	public String board() {
+		
+		return "board"; // jsp파일명
+	}
+	
+}
